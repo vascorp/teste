5bd0db9f3b32ee581d4370276145e12c84ed7fdf
Vasco Pereira <vascorp@gmail.com>
2020-12-27 20:21:21 +0000

    Add files via upload

--- calculator2.js
+++ calculator2.js
@@ -886,7 +886,7 @@ function SalarioLiquidoCtrl($scope, $timeout) {
             else
                 result.subsidio_refeicao = input.refeicao_valor * input.refeicao_dias;
 
-            result.subsidio_refeicao = result.subsidio_refeicao * 11; //11meses
+            //result.subsidio_refeicao = result.subsidio_refeicao * 11; //11meses
             //console.log('result.subsidio_refeicao', result.subsidio_refeicao)
             result.tributavel += Math.max(input.refeicao_valor - input.refeicao_tipo.isento, 0) * input.refeicao_dias;
             result.tributavel_coverflex += Math.max(input.refeicao_valor - input.refeicao_tipo.isento, 0) * input.refeicao_dias;
