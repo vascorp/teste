3aa5aedc352090e17b2c4336e0d360433a82a363
Vasco Pereira <vascorp@gmail.com>
2021-03-04 22:57:33 +0000

    Add files via upload

--- wodify-intranet.js
+++ wodify-intranet.js
@@ -1,13 +1,11 @@
-
-console.log('first');
 function fixMenu (){
     var links = document.getElementsByTagName('a');
-
+    console.log(links);
+    console.log(window.top.document.getElementsByTagName('a'));
     for(var i = 0; i< links.length; i++){
         console.log('links[i].href.', links[i].href);
         if(links[i].href.includes("/sub-nav")) links[i].setAttribute('href', "#");
         if(links[i].href.includes("#")) links[i].setAttribute('data-url', "#");
     }
 }
-console.log(this);
 fixMenu();
